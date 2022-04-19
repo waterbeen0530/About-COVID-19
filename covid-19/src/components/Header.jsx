fc606a84d17c6f95734d2ddd33e6a63b1f654e1b
waterbeen0530 <hange253@gmail.com>
2022-04-19 23:37:08 +0900

    [CREATE] Add header
    Header 추가

---
+++ covid-19/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.span`
+  margin-left: 7px;
+  font-weight: 600;
+`;
+
+function Header() {
+  return (
+    <>
+      <Title>코로나바이러스감영증-19(COVID-19)</Title>
+      "정보전달용"
+    </>
+  );
+}
+
+export default Header;
