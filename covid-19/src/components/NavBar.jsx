7583a792da786b444f0f782f0cf9fa601754f120
waterbeen0530 <hange253@gmail.com>
2022-04-26 23:35:32 +0900

    [UPDATE] drop-down menu
    NavVar 수정

--- covid-19/src/components/NavBar.jsx
+++ covid-19/src/components/NavBar.jsx
@@ -3,7 +3,12 @@ import styles from "../styles/NavBar.module.css";
 import styled from "styled-components";
 
 const Container = styled.div`
-  background: #064772;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  a {
+    text-decoration: none;
+  }
 `;
 
 const NavVar = styled.div`
@@ -13,14 +18,15 @@ const NavVar = styled.div`
 `;
 
 const NavMenu = styled.ul`
-  width: 11180px;
-  margin: 0 0;
-  padding: 21px 300px 16px 300px;
+  width: 1180px;
+  margin: 0;
+  padding: 21px 0 16px 0;
   display: table;
   table-layout: fixed;
   li {
     display: table-cell;
     list-style: none;
+    text-align: center;
   }
   a {
     color: #fff;
@@ -29,86 +35,120 @@ const NavMenu = styled.ul`
 
 const DropVar = styled.div`
   width: 1180px;
-  margin: 0 auto;
-  padding: 0 300px;
+  //margin: 0 auto;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   display: table;
   table-layout: fixed;
 `;
 
 const DropDown = styled.ul`
-  padding: 0 auto;
+  margin: 0 auto;
+  padding: 0 0;
   display: table-cell;
 
-  a {
-    color: #fff;
+  li {
+    padding-bottom: 10px;
+    list-style: none;
+    text-align: center;
+    font-size: 14px;
   }
+
+  a {
+    color: #000;
+  }
+`;
+
+const NavWrapper = styled.div`
+  width: 100%;
+  background: #064772;
+  &:hover {
+    DropWrapper ul li {
+      display: block;
+    }
+  }
+`;
+
+const DropWrapper = styled.div`
+  width: 100%;
+  padding-top: 15px;
+  display: none;
+  justify-content: center;
+  align-items: center;
+  background: #e1e2e3;
 `;
 
 function NavBar() {
   return (
     <Container className={styles.menu}>
-      <NavVar>
-        <NavMenu>
-          <li>
-            <a href="">국내 전체</a>
-          </li>
-          <li>
-            <a href="">국내 예방 접종 현황</a>
-          </li>
-          <li>
-            <a href="">국내 카운터</a>
-          </li>
-          <li>
-            <a href="">시도별 발생 현황</a>
-          </li>
-        </NavMenu>
-      </NavVar>
-      <DropVar>
-        <DropDown>
-          <li>
-            <a href="">menu1</a>
-          </li>
-          <li>
-            <a href="">menu2</a>
-          </li>
-          <li>
-            <a href="">menu3</a>
-          </li>
-        </DropDown>
-        <DropDown>
-          <li>
-            <a href="">menu1</a>
-          </li>
-          <li>
-            <a href="">menu2</a>
-          </li>
-          <li>
-            <a href="">menu3</a>
-          </li>
-        </DropDown>
-        <DropDown>
-          <li>
-            <a href="">menu1</a>
-          </li>
-          <li>
-            <a href="">menu2</a>
-          </li>
-          <li>
-            <a href="">menu3</a>
-          </li>
-        </DropDown>
-        <DropDown>
-          <li>
-            <a href="">menu1</a>
-          </li>
-          <li>
-            <a href="">menu2</a>
-          </li>
-          <li>
-            <a href="">menu3</a>
-          </li>
-        </DropDown>
-      </DropVar>
+      <NavWrapper>
+        <NavVar>
+          <NavMenu>
+            <li>
+              <a href="">국내 전체</a>
+            </li>
+            <li>
+              <a href="">국내 예방 접종 현황</a>
+            </li>
+            <li>
+              <a href="">국내 카운터</a>
+            </li>
+            <li>
+              <a href="">시도별 발생 현황</a>
+            </li>
+          </NavMenu>
+        </NavVar>
+      </NavWrapper>
+      <DropWrapper>
+        <DropVar>
+          <DropDown>
+            <li>
+              <a href="">menu1</a>
+            </li>
+            <li>
+              <a href="">menu2</a>
+            </li>
+            <li>
+              <a href="">menu3</a>
+            </li>
+          </DropDown>
+          <DropDown>
+            <li>
+              <a href="">menu1</a>
+            </li>
+            <li>
+              <a href="">menu2</a>
+            </li>
+            <li>
+              <a href="">menu3</a>
+            </li>
+          </DropDown>
+          <DropDown>
+            <li>
+              <a href="">menu1</a>
+            </li>
+            <li>
+              <a href="">menu2</a>
+            </li>
+            <li>
+              <a href="">menu3</a>
+            </li>
+          </DropDown>
+          <DropDown>
+            <li>
+              <a href="">menu1</a>
+            </li>
+            <li>
+              <a href="">menu2</a>
+            </li>
+            <li>
+              <a href="">menu3</a>
+            </li>
+          </DropDown>
+        </DropVar>
+      </DropWrapper>
     </Container>
   );
 }
