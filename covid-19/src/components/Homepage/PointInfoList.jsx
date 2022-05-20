ea5f8168ff97ea12262d5bbbcc4868efbeaaa5b4
waterbeen0530 <hange253@gmail.com>
2022-05-20 23:53:22 +0900

    [UPDATE] PointInfoList
    UI 수정

--- covid-19/src/components/Homepage/PointInfoList.jsx
+++ covid-19/src/components/Homepage/PointInfoList.jsx
@@ -1,62 +1,143 @@
 import React from "react";
 import styled from "styled-components";
 
-const List = styled.div`
+const Container = styled.div`
+  z-index: 50;
   position: absolute;
-
-  background: #000;
+  top: 55%;
+  left: 24%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Item = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  font-size: 36px;
+const Wrapper = styled.div`
+  width: auto;
+  table-layout: fixed;
+  /* border: 1px solid red;
+  animation-delay: 0.6s;
+  animation-name: downMotion;
+  animation-duration: 0.5s;
+  animation-timing-function: linear;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+  transition: all 0.5s ease 0s;
+  @keyframes downMotion {
+    0% {
+      transform: translateY(-100px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0px);
+      opacity: 1;
+    }
+  } */
 `;
 
-const A1 = styled.a`
-  background: #dfebf1;
-`;
+const Table = styled.div`
+  width: 100%;
+  display: flex;
+  div {
+    width: 346px;
+    height: 340px;
+    margin: 10px 23px;
+    display: flex;
+    justify-content: center;
+    border: solid 3px #637291;
+    border-radius: 50px;
+    background-color: #dfebf1;
+    font-size: 40px;
+    box-shadow: 5px 5px 5px rgb(100 100 100 / 30%);
 
-const A2 = styled.a`
-  background: #f8f8f8;
+    p {
+      margin: 36px 0 36px;
+      text-align: center;
+    }
+    #firstLink {
+      background: url("https://ncv.kdca.go.kr/ncov/img/pointInfo_img_01.png")
+        no-repeat;
+      background-position: center bottom;
+      background-size: 166px 140px;
+      animation-delay: 0.5s;
+      animation-name: downMotion;
+      animation-duration: 0.5s;
+      animation-timing-function: linear;
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
+      transition: all 0.5s ease 0s;
+    }
+    #SecondLink {
+      background: url("https://ncv.kdca.go.kr/ncov/img/pointInfo_img_02.png")
+        no-repeat;
+      background-position: center bottom;
+      background-size: 146px 153px;
+      animation-delay: 0.7s;
+    }
+    #ThirdLink {
+      background: url("https://ncv.kdca.go.kr/ncov/img/pointInfo_img_04.png")
+        no-repeat;
+      background-position: center bottom;
+      background-size: 216px 142px;
+      animation-delay: 0.9s;
+    }
+    #FourthLink {
+      background: url("	https://ncv.kdca.go.kr/ncov/img/pointInfo_img_03.png")
+        no-repeat;
+      background-position: center bottom;
+      background-size: 244px 160px;
+      animation-delay: 1.1s;
+    }
+    span {
+      font-weight: 700 !important;
+    }
+  }
+  #SecondBox {
+    background-color: #f8f8f8;
+  }
 `;
 
 function PointInfoList() {
   return (
-    <List>
-      <Item>
-        <A1 href="">
-          <h3>
-            <span>코로나19</span>와<span>오미크론 변이</span>
-          </h3>
-        </A1>
-        <A2 href="">
-          <h3>
-            코로나19<span>감염 예방</span>
-          </h3>
-        </A2>
-        <A1 href="">
-          <h3>
-            코로나19<span>검사</span>
-          </h3>
-        </A1>
-        <A2 href="">
-          <h3>
-            <span>코로나19 감염</span>되었을때
-          </h3>
-        </A2>
-        <A1 href="">
-          <h3>
-            <span>재택치료</span>
-          </h3>
-        </A1>
-        <A2 href="">
-          <h3>
-            <span>접촉자 자가격리</span>
-          </h3>
-        </A2>
-      </Item>
-    </List>
+    <Container>
+      <Wrapper>
+        <Table>
+          <div>
+            <p id="firstLink">
+              <span>국내 </span>
+              코로나19
+              <br />
+              <span>발생 현황</span>
+            </p>
+          </div>
+          <div id="SecondBox">
+            <p id="SecondLink">
+              <span>국내 </span>
+              코로나19
+              <br />
+              <span>예방 접종 현황</span>
+            </p>
+          </div>
+        </Table>
+        <Table>
+          <div id="SecondBox">
+            <p id="ThirdLink">
+              <span>국내 </span>
+              코로나 19
+              <br />
+              <span>카운터</span>
+            </p>
+          </div>
+          <div>
+            <p id="FourthLink">
+              <span>시도별 </span>
+              코로나 19
+              <br />
+              <span>발생 동향</span>
+            </p>
+          </div>
+        </Table>
+      </Wrapper>
+    </Container>
   );
 }
 
